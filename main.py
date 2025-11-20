da4f50db673122d18280543fa7feec0e945ae10e
CristóbalO_DonKronos <56177605+CristobalKronos@users.noreply.github.com>
2025-11-20 12:30:22 -0300

    Create main
    Se crea el archivo main.py con una estructura básica para inicializar el trabajo.

---
+++ main.py
@@ -0,0 +1,26 @@
+''' MAIN README
+
+    # Librerías recomendadas para investigar:
+    - Pygame
+    - Arcade
+    - Godot
+    - Panda3D
+    - Ursina Engine
+
+'''
+
+# IMPORT'S
+import os # Librería para ejecutar comandos de consola.
+
+
+# FUNCTIONS
+
+
+# MAIN FUNCTION
+def main():
+    print("Starting...")
+    print("...Ending")
+    return()
+
+# INITIALIZING
+main()
