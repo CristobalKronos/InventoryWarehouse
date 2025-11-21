01bb528af46f27e7305d49aed48161737502cc8b
CristóbalO_DonKronos <56177605+CristobalKronos@users.noreply.github.com>
2025-11-21 17:31:41 -0300

    Create Seeders
    Se crea el archivo seeder con un pequeño roadmap comentado.

---
+++ Seeders/seederUsers.py
@@ -0,0 +1,15 @@
+''' SEEDERUSERS README
+
+    El objetivo es inicializar los usuarios
+
+'''
+
+# IMPORT'S
+## Importar la clase de usuario y de usuarios.
+
+# FUNCTIONS
+## Crear el objeto listado de usuarios
+## Abrir el fichero de usuarios
+## Crear el método de creación de entidades usuario
+## Asignar los usuarios al listado maestro de usuarios.
+## Retornar el listado maestro de usuarios
